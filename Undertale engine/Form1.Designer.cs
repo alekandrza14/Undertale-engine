3d2b5d783c6c56dcbe831030a0663b6e5a34562a
unauticna <66287905+alekandrza14@users.noreply.github.com>
2022-10-25 12:05:02 +0500

    Добавьте файлы проекта.

---
+++ Undertale engine/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Undertale_engine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(515, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "hp:78964";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(515, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "name";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackgroundImage = global::Undertale_engine.Resource1.fight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(13, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "i1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImage = global::Undertale_engine.Resource1.Sans;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(314, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 278);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 800;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(839, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Undertale engine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Undertale engine/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unauticna;
+using System.IO;
+
+namespace Undertale_engine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image tex7 = Resource1.Sans;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            unscript un = new unscript();
+            if (File.Exists("main.uns"))
+            {
+
+
+                un.ins = "main.uns";
+                un.Start();
+                List<Image> n = new List<Image>();
+                int i = 0;
+                if (un.outsv.Count != 0)
+                {
+                    enemyhp = (decimal)un.outsv[0].x;
+                    while ((int)un.outsv[0].y > i)
+                    {
+
+
+                        if (un.link.Count != 0)
+                        {
+
+                            string path;
+                            path = un.link[i + 1];
+                            n.Add(Bitmap.FromFile(path));
+                            pictureBox1.BackgroundImage = tex7;
+                            i++;
+                        }
+                    }
+                }
+                if (un.outsd.Count != 0)
+                {
+
+
+                    enemyhp = (decimal)un.outsd[0].x;
+                    while ((int)un.outsd[0].y > i)
+                    {
+
+
+                        if (un.link.Count != 0)
+                        {
+
+                            string path;
+                            path = un.link[i+1];
+                            
+                            n.Add(Bitmap.FromFile(path));
+                            pictureBox1.BackgroundImage = tex7;
+                            i++;
+                        }
+                    }
+                }
+                textr = n.ToArray();
+                if (un.outs2.Count != 0)
+                {
+                    enemyname = un.outs2[0];
+                }
+                if (un.link.Count != 0)
+                {
+
+                    string path = un.link[0];
+                    tex7 = Bitmap.FromFile(path);
+                }
+                
+            }
+            pictureBox1.BackgroundImage = tex7;
+            
+        }
+
+            //////////////////////////////////////////////////// 
+
+            decimal enemyhp = 78964m;
+        string enemyname = "not_in_au_sans";
+
+
+        Image[] textr = new Image[] { Resource1.Sans_порожение_1, Resource1.Sans_порожение__1 };
+        short animatic;
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "hp:" + enemyhp.ToString();
+            label2.Text = enemyname;
+            if (enemyhp <= 0)
+            {
+                label1.Text = "dead";
+
+
+                pictureBox1.BackgroundImage = textr[animatic];
+            }
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            animatic += 1;
+            if (textr.Length - 1 < animatic)
+            {
+                animatic = 0;
+            }
+        }
+
+        //////////////////////////////////////////////////// 
+
+
+
+        //////////////////////////////////////////////////// 
+        //КНОПКА ТЕСТ 1
+        Bitmap tex1 = Resource1.fight;
+        Bitmap tex2 = Resource1.fight_click;
+        Bitmap tex3 = Resource1.fight_unclick;
+        Bitmap tex4 = Resource1.fight_enter;
+
+        bool onconected;
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackgroundImage = tex4;
+            onconected = false;
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button1.BackgroundImage = tex2;
+            onconected = true;
+            if (enemyhp >=0)
+            {
+
+
+                enemyhp -= 123m;
+            }
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button1.BackgroundImage = tex3;
+            onconected = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (onconected)
+            {
+
+
+                button1.BackgroundImage = tex1;
+                onconected = false;
+
+            }
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackgroundImage = tex3;
+            onconected = true;
+            
+        }
+
+        
+
+
+        //////////////////////////////////////////////////// 
+
+
+
+    }
+}
